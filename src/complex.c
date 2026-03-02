5c0bb0aba747b8aeb0ea6e061ee1f279900d51ed
Sriram Katta <sriram.katta.939@gamil.com>
2026-03-02 15:31:07 +0100

    Fix Carg function to correctly compute the argument of a complex number

--- src/complex.c
+++ src/complex.c
@@ -44,7 +44,7 @@ complex Cdiv(const complex *c1, const complex *c2)
   CG_FLOAT x       = c2->real;
   CG_FLOAT y       = c2->imag;
   CG_FLOAT c2_abs2 = Cabs2(c2);
-  return make_complex((u * x + v * y) / c2_abs2, (v * x + u * y) / c2_abs2);
+  return make_complex((u * x + v * y) / c2_abs2, (v * x - u * y) / c2_abs2);
 }
 
 complex Cinv(const complex *c1)
@@ -52,7 +52,7 @@ complex Cinv(const complex *c1)
   CG_FLOAT x       = c1->real;
   CG_FLOAT y       = c1->imag;
   CG_FLOAT c1_abs2 = Cabs2(c1);
-  return make_complex((x) / c1_abs2, (y) / c1_abs2);
+  return make_complex((x) / c1_abs2, (-y) / c1_abs2);
 }
 
 complex Cconj(const complex *c1)
@@ -72,13 +72,13 @@ CG_FLOAT Cabs(const complex *c1)
 CG_FLOAT Cabs2(const complex *c1)
 {
   CG_FLOAT x = c1->real;
-  CG_FLOAT y = c1->real;
+  CG_FLOAT y = c1->imag;
   return (x * x) + (y * y);
 }
 
 CG_FLOAT Carg(const complex *c1)
 {
-  const CG_FLOAT ratio = c1->imag / c1->imag;
+  const CG_FLOAT ratio = c1->imag / c1->real;
 #if PRECISION == 1
   return tanf(ratio)
 #else
