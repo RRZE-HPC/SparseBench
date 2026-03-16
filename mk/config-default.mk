befcc1e732695479745eff5382c62f2d53455b29
Sriram Katta <sriram.katta@fau.de>
2026-03-16 09:14:25 +0100

    Update config-default.mk: change toolchain to NVCC, adjust chunk size, and refine complex element support

--- mk/config-default.mk
+++ mk/config-default.mk
@@ -1,16 +1,27 @@
-# Supported: GCC, CLANG, ICX
-TOOLCHAIN ?= CLANG
+# Supported: GCC, CLANG, ICX, NVCC, HIP
+TOOLCHAIN ?= NVCC
 # Supported CRS, SCS, CCRS
 MTX_FMT ?= SCS
 ENABLE_MPI ?= false
-ENABLE_OPENMP ?= true
+ENABLE_OPENMP ?= false
 FLOAT_TYPE ?= DP # SP for float, DP for double
 UINT_TYPE ?= U # U for unsigned int, ULL for unsigned long long int
-SELL_CHUNK_VALUE ?= 32
+USE_COMPLEX_ELEMENTS ?= false
+SELL_CHUNK_VALUE ?= 64
 SELL_SIGMA_VALUE ?= 64
 NUM_VEC ?= 10
-USE_COMPLEX_ELEMENTS ?= false
-
+# GPU architecture (only used when TOOLCHAIN=NVCC or HIP)
+# # NVCC: 
+#       -gencode=arch=compute_80,code=sm_80 # for A100
+#       -gencode=arch=compute_86,code=sm_86 # for A40
+#       -gencode=arch=compute_90,code=sm_90 # for GH200
+CUDA_ARCH ?= -gencode=arch=compute_80,code=sm_80 -gencode=arch=compute_86,code=sm_86 -gencode=arch=compute_90,code=sm_90
+# # HIP:  
+#       gfx908 # for MI100
+#       gfx90a # for MI210A
+#       gfx1030 # for RX 6900 XT
+#       gfx942 # for MI300X & MI300A
+HIP_ARCH  ?= gfx1030,gfx942
 #Feature options
 OPTIONS +=  -DARRAY_ALIGNMENT=64
 OPTIONS +=  -DOMP_SCHEDULE=static
@@ -43,7 +54,6 @@ ifeq ($(strip $(MTX_FMT)),SCS)
     DEFINES += -DSELL_CHUNK=$(SELL_CHUNK_VALUE)
     DEFINES += -DSELL_SIGMA=$(SELL_SIGMA_VALUE)
 endif
-
 ifeq ($(strip $(USE_COMPLEX_ELEMENTS)),true)
     DEFINES += -DUSE_COMPLEX
 endif
