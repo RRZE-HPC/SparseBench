f3f0c011e75da672471df682749a0428a7c29052
Jan Eitzinger <jan@moebiusband.org>
2026-04-28 09:11:22 +0200

    Unify compiler flags

--- mk/include_ICX.mk
+++ mk/include_ICX.mk
@@ -12,7 +12,7 @@ OPENMP   = -qopenmp
 endif
 
 VERSION  = --version
-CFLAGS   =  -O3 -g -ffast-math -xHost -fno-alias -fno-inline -Xclang -target-feature -Xclang +prefer-no-gather $(OPENMP) # -std=c23
+CFLAGS   =  -O3 -ffast-math -xHost -fno-alias -fno-inline -Xclang -target-feature -Xclang +prefer-no-gather $(OPENMP)
 # CFLAGS   = -O0 -g -std=c99 $(OPENMP)
 LFLAGS   = $(OPENMP)
 DEFINES  += -D_GNU_SOURCE # -DVERBOSE
